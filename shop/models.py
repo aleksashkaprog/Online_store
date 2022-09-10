7fb14ab6a948ec0c3ccd24aeed49e262d20c458e
Максим Наумович <the7fame@gmail.com>
2022-09-10 17:19:03 +0300

    model vendor

--- shop/models.py
+++ shop/models.py
@@ -1,7 +1,36 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
 class Shop(models.Model):
     """Магазин"""
+
     name = models.CharField(max_length=512, verbose_name=_("название"))
+    holder = models.ForeignKey(User, on_delete=models.CASCADE, related_name="vendor", verbose_name=_("владелец"))
+    address = models.TextField(blank=True, null=True, verbose_name=_("адрес"))
+    email = models.EmailField(blank=True, null=True, verbose_name=_("почта"))
+    phone = models.CharField(max_length=16, blank=True, null=True, verbose_name=_("телефон"))
+    description = models.TextField(blank=True, null=True, verbose_name=_("описание"))
+    logo = models.ImageField(upload_to="/logo", null=True, verbose_name=_("логотип"))
+    slug = models.SlugField(unique=True, verbose_name=_("слаг"))
+
+    def __str__(self):
+        return f"Магазин {self.name}"
+
+
+class ShopProduct(models.Model):
+    """Модель продукта магазина"""
+
+    store = models.ForeignKey(
+        "Shop", on_delete=models.CASCADE, related_name="vendor_products", verbose_name=_("магазин")
+    )
+    product = models.ForeignKey(
+        "Product", on_delete=models.CASCADE, related_name="vendor_product", verbose_name=_("продукт")
+    )
+    price = models.DecimalField(max_digits=12, decimal_places=2, verbose_name=_("цена"))
+    amount = models.IntegerField(default=0, verbose_name=_("количество"))
+    add_at = models.DateField(auto_now_add=True, verbose_name=_("дата добавления"))
+
+    def __str__(self):
+        return f"Продукт {self.product} из магазина {self.store}"
