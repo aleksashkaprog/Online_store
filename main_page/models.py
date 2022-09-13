8d0c7da991cca29b8188f6cc487cf28035c70768
aleksashkaprog <lalalalala.7la@gmail.com>
2022-09-13 09:27:00 +0800

    model_banner

--- main_page/models.py
+++ main_page/models.py
@@ -1,3 +1,9 @@
-# from django.db import models
+from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from product.models import Product
+
+
+class Banner(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="banners", verbose_name=_("товар"))
+    logo = models.ImageField(upload_to="/logo", null=True, verbose_name=_("логотип"))
