8c868068670a64f52ae43acd4d6582e8e844d594
aleksashkaprog <lalalalala.7la@gmail.com>
2022-09-20 01:17:06 +0800

    main_banner

--- main_page/views.py
+++ main_page/views.py
@@ -1,3 +1,17 @@
 # from django.shortcuts import render
+import random
 
-# Create your views here.
+from django.shortcuts import render
+from django.views import View
+
+from .models import Banner
+
+
+class MainView(View):
+    def get(self, request):
+        banner_list = []
+        banner = Banner.objects.all()
+        for ban in banner:
+            banner_list.append(ban)
+        my_banner = random.sample(banner_list, 3)
+        return render(request, 'main.html', context={'my_banner': my_banner})
