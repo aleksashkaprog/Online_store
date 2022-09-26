974ac04e550a83654d31e5ba5d3bc61645321914
Максим Наумович <the7fame@gmail.com>
2022-09-26 17:20:38 +0300

    pages profile, account

--- personal_account/views.py
+++ personal_account/views.py
@@ -7,5 +7,6 @@ from django.views.generic import TemplateView
 class AccountView(TemplateView):
     template_name = 'users/account.html'
 
+
 class ProfileView(TemplateView):
     template_name = 'users/profile.html'
