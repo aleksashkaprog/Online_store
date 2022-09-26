6ea12f0a4c85f9182f517f439b1fe8d604fbb1d5
maksimpolivnikov <heavy97@yandex.ru>
2022-09-26 14:08:48 +0800

    Finished working with custom user

--- main_page/views.py
+++ main_page/views.py
@@ -9,9 +9,10 @@ from main_page.models import Banner
 class MainPageView(View):
 
     def get(self, request):
-        banner_list = []
-        banner = Banner.objects.all()
-        for ban in banner:
-            banner_list.append(ban)
-        my_banner = random.sample(banner_list, 3)
-        return render(request, 'main.html', context={'my_banner': my_banner})
+        # banner_list = []
+        # banner = Banner.objects.all()
+        # for ban in banner:
+        #     banner_list.append(ban)
+        # my_banner = random.sample(banner_list, 3)
+        my_banner = None
+        return render(request, 'main_page.html', context={'banners': my_banner})
--- product/urls.py
+++ product/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import Product
 
 urlpatterns = [
-    path("", Product.as_view(), name="product"),
+    path("", Product.as_view(), name="products"),
 ]
