2b12e78397813fca05c1f01b423785735b2cb1d6
the7fame@gmail.com <the7fame@gmail.com>
2022-10-20 23:07:25 +0300

    order

--- order/models.py
+++ order/models.py
@@ -23,6 +23,8 @@ class Order(models.Model):
     cost_delivery = models.DecimalField(
         max_digits=10, decimal_places=2, default=0, verbose_name=_("стоимость доставки")
     )
+    order_in = models.BooleanField(default=False, verbose_name=_("в заказе"))
+    ordered = models.DateTimeField(null=True, blank=True, verbose_name=_("время заказа"))
 
     def __str__(self):
         return f"Order №{self.id}"
@@ -37,17 +39,26 @@ class Order(models.Model):
             sum += good.quantity * good.seller_good.price
         return sum
 
+    @property
+    def all_goods_price_disc_delivery(self):
+        sum = 0
+        for good in self.order_goods.all():
+            sum += good.quantity * good.price_eventual
+        return sum + self.cost_delivery
+
     class Meta:
         verbose_name = _("заказ")
         verbose_name_plural = _("заказы")
 
 
 class OrderGood(models.Model):
-    good = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="order_goods", verbose_name=_("продукт"))
+    good = models.ForeignKey(
+        Order, on_delete=models.CASCADE, related_name="order_goods", verbose_name=_("продукт в заказе")
+    )
     seller_good = models.ForeignKey(
         ShopProduct, on_delete=models.CASCADE, related_name="order_goods", verbose_name=_("продавец")
     )
-    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=_("цена"))
+    price_eventual = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=_("цена"))
     quantity = models.IntegerField(default=1, verbose_name=_("количество"))
 
     def __str__(self):
